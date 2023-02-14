066b75c4ae30770a4df364604b594c285e3d2593
Alvin Kannalath <akannalath@gmail.com>
2023-02-14 15:32:12 -0500

    All done

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
+/*Allows the points on the grid to be clicked and allows the button to be 
+clicked to add more points*/
 function toggle(){
 	this.classList.toggle("clicked");
-
 }
 
+//This function allows the user to see what the last point clicked was
 function get_coordinates(){
 	let x = (this.getAttribute('cx') - 100) / 40;
 	let y = 10 - ((this.getAttribute('cy') - 100) / 40);
 
-	let coordinate = `(${x}, ${y})`;
+	let coordinate = "Last Point Clicked: " + `(${x}, ${y})`;
 	document.getElementById("lastClicked").innerHTML = coordinate;
 }
 
-
+/*Allows the cicles to be clicked which will then give them a border and tell what the
+coordinate of that point was*/
 function borders() {
 	const circles = document.getElementsByTagName("circle");
 
@@ -19,24 +22,24 @@ function borders() {
 		circles[i].addEventListener("click", toggle);
 		circles[i].addEventListener("click", get_coordinates);
 	}
-
 }
 
+/*This allows the circles to be clicked before a new point is added to the grid so that it
+is not dependent on the button feature*/
 (function(){
 	borders();
 })();
 
-
-
+//Allows a circle to be added to the grid based on the user entries in the dropdown menu
 function add_circle() {
 	let frame = document.getElementById("frame");
 
-	// Input numbers
+	// Based on the user entry in the dropdown, it will proportionally add it to the grid
 	let x = 100 + ((document.getElementById("xCoordinate").value) * 40) ;
 	let y = 500 - ((document.getElementById("yCoordinate").value) * 40);
 
 
-	// Create SVG circle element
+	// Create the SVG circle element
 	let circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
 	circle.setAttribute("cx", x);
 	circle.setAttribute("cy", y);
@@ -48,13 +51,6 @@ function add_circle() {
 	borders()
 }
 
+//Adds an event listener for the adding circle function
 document.getElementById("addPoint")
 		.addEventListener('click', add_circle);
-
-
-
-
-
-
-	
-
